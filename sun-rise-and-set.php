ca94ba1b83c905a92e1b63d7bf06e3ca88175cff
Beda Schmid <arbeit@yoonect.com>
2020-06-07 18:17:01 +0700

    Small typo

--- sun-rise-and-set.php
+++ sun-rise-and-set.php
@@ -3,18 +3,19 @@
  * Plugin Name: Sun Rise and Sun Set
  * Description: Plotting Sun Rise and Sun Set according to User Location
  * Plugin URI: http://www.tukutoi.com
- * Author: Author
- * Author URI: http://www.tukutoi.com
+ * Author: TukuToi
+ * Author URI: http://www.tukutoi.com/author/Beda_Schmid
  * Version: 1.0.0
  * License: GPL2
  * Text Domain: tkt-srss
  */
 ?>
+
 <script>
-var d = new Date();
-var n = d.getTimezoneOffset();
-var name = "timezone";
-document.cookie = name + "=" + n/60;
+	var d = new Date();
+	var n = d.getTimezoneOffset();
+	var name = "timezone";
+	document.cookie = name + "=" + n/60;
 </script>
 
 <?php
@@ -34,7 +35,11 @@ document.cookie = name + "=" + n/60;
 
 		$sunset = date_sunset(time(), SUNFUNCS_RET_STRING, $lat, $long, 90, $offset);
 		$sunrise =  date_sunrise(time(), SUNFUNCS_RET_STRING, $lat, $long, 90, $offset);
+		
 		$out = 'Today Sun rose at '.$sunrise.' and it will set at '.$sunset;
+		
 		return $out;
+		
 	}
+
 	add_shortcode( 'sunrise-and-set', 'display_sunrise_and_set' );
